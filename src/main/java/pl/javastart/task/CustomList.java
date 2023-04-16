e5db204e8fac59897d6d31d71610b0a6c9a625fe
Jan Termion <jan.termion@gmail.com>
2023-04-16 22:24:27 +0200

    wyglada na to, ze wszystko dziala

--- src/main/java/pl/javastart/task/CustomList.java
+++ src/main/java/pl/javastart/task/CustomList.java
@@ -4,127 +4,59 @@ import java.util.*;
 
 // Uzupełnij klasę. Możesz ją dowolnie modyfikować.
 // Celem jest, żeby przechodziły testy w src/test/java/[...]/CustomListTest
-public class CustomList<T> implements List<T> {
+public class CustomList<T>  {
 
-    private final List<T> list = new ArrayList<>();
+    private T[] arrayOfData;
 
-    @Override
+    public CustomList() {
+        arrayOfData = (T[]) new Object[0];
+    }
+
+    public void add(T a) {
+        arrayOfData = Arrays.copyOf(arrayOfData, arrayOfData.length + 1);
+        arrayOfData[arrayOfData.length - 1] = a;
+    }
+
+    public void add(int i, T a1) {
+
+        if (i > arrayOfData.length || i < 0) {
+            throw new IndexOutOfBoundsException("element tablicy o takim numerze nie moze byc utworzony");
+        }
+
+        if ((arrayOfData.length + 1) == 1) {
+            add(a1);
+        } else {
+            arrayOfData = Arrays.copyOf(arrayOfData, arrayOfData.length + 1);
+            for (int j = arrayOfData.length - 1; j > i; j--) {
+                arrayOfData[j] = arrayOfData[j - 1];
+            }
+            arrayOfData[i] = a1;
+        }
+    }
+
+    public T get(int i) {
+        return arrayOfData[i];
+    }
+
     public int size() {
-        return list.size();
+        return arrayOfData.length;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T1> T1[] toArray(T1[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(T t) {
-        return list.add(t);
-    }
-
-    @Override
-    public void add(int index, T element) {
-        list.add(index, element);
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return list.remove(o);
-    }
-
-    @Override
-    public T remove(int index) {
-        return list.remove(index);
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends T> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public T get(int index) {
-        return list.get(index);
-    }
-
-    @Override
-    public T set(int index, T element) {
-        return null;
-    }
-
-    @Override
-    public int indexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return 0;
-    }
-
-    @Override
-    public ListIterator<T> listIterator() {
-        return null;
-    }
-
-    @Override
-    public ListIterator<T> listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List<T> subList(int fromIndex, int toIndex) {
-        return null;
+    public void remove(int i) {
+        if (i + 1 > arrayOfData.length || i < 0) {
+            throw new IndexOutOfBoundsException("element tablicy o takim numerze nie istnieje");
+        }
+        if (i + 1 < arrayOfData.length) {
+            for (int j = i + 1; j < arrayOfData.length; j++) {
+                arrayOfData[j - 1] = arrayOfData[j];
+            }
+        }
+        arrayOfData = Arrays.copyOf(arrayOfData, arrayOfData.length - 1);
     }
 
     @Override
     public String toString() {
-        return list.toString();
+        return Arrays.toString(arrayOfData);
     }
 }
+
